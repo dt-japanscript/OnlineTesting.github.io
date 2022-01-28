3888575fc984ba0193732beee2bf94e820bb8c49
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-01-28 09:54:09 +0900

    Build main.js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let questionList = [];
+
+const domID = (id) => {
+    return document.getElementById(id);
+}
+
+const fetchQuestion = async () => {
+    try {
+        const res = await axios({
+            url: "https://5bd2959ac8f9e400130cb7e9.mockapi.io/api/questions",
+            method: "GET",
+        });
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const renderQuestion = () => {
+    let htmlContent = '';
+    for (let item of questionList) {
+        htmlContent += item.render();
+    }
+
+    domID("questionsContainer").innerHTML = htmlContent;
+};
+
+const mapData = (data = []) => {
+    questionList = data.map((item) => {
+        const { questionType, id, content, answer } = item; // destructuring in ES6
+        if (item.questionType === 1) {
+            return new MultipleChoice(questionType, id, content, answer);
+        } else {
+            return new FillInBlank(questionType, id, content, answer);
+        }
+    });
+}
+
+fetchQuestion().then((res) => {
+    // code khi question list da co
+    mapData(res);
+    renderQuestion();
+});
--- models/fillInBlank.js
+++ models/fillInBlank.js
@@ -1,9 +1,9 @@
-class FillBlank extends Question {
-    constructor(type, id, content, answer) {
-        super(type, id, content, answer);
-    }
-    render(index) {
-        return `
+class FillInBlank extends Question {
+  constructor(type, id, content, answer) {
+    super(type, id, content, answer);
+  }
+  render(index) {
+    return `
           <div>
             <p class="lead font-italic" style="font-size: 30px;">
               Câu ${index}: ${this.content}
@@ -11,5 +11,5 @@ class FillBlank extends Question {
            <input id="answer-${this.id}" type="text" class="form-control w-50" />
           </div>
           `;
-    }
+  }
 }
